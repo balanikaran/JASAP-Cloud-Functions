11324d7a2b1360b61f21c3a44db4f3ebba87312f
krnblni <karanbalani007@gmail.com>
2020-05-20 04:02:06 +0530

    added authentication middleware (partially)

--- functions/index.js
+++ functions/index.js
@@ -25,6 +25,48 @@ const app = express();
 // to simply call db for database...
 const db = admin.firestore();
 
+// Firebase Authorization Middleware
+const firebaseAuthMiddleware = (request, response, next) => {
+    let userToken;
+    if (
+        request.headers.authorization &&
+        request.headers.authorization.startsWith("Bearer ")
+    ) {
+        userToken = request.headers.authorization.split("Bearer ")[1];
+    } else {
+        console.error("no token found...");
+        return response.status(403).json({
+            error: "unauthorized",
+        });
+    }
+
+    // now we have our token
+    // we can now validate the token
+    admin
+        .auth()
+        .verifyIdToken(userToken)
+        .then((decodedUserToken) => {
+            console.log(decodedUserToken);
+            request.user = decodedUserToken;
+            return db
+                .collection("users")
+                .where("uid", "==", request.user.uid)
+                .limit(1)
+                .get();
+        })
+        .then((data) => {
+            request.user.username = data.docs[0].data().username;
+            // next is called here means token is valid
+            // we can proceed for whatever task we sent the request for
+            return next();
+        })
+        .catch((err) => {
+            console.error(err);
+            // err is already a json
+            return response.status(403).json(err);
+        });
+};
+
 // Get posts route
 app.get("/getPosts", (request, response) => {
     db.collection("posts")
@@ -46,9 +88,13 @@ app.get("/getPosts", (request, response) => {
 });
 
 // Create new post route
-app.post("/createPost", (request, response) => {
+app.post("/createPost", firebaseAuthMiddleware, (request, response) => {
+    // request now has a user property
+    // this is added via middleware
+    // upon successful validation of token
+
     const newPost = {
-        username: request.body.username,
+        username: request.user.username,
         body: request.body.body,
         createdAt: new Date().toISOString(),
     };
@@ -152,6 +198,7 @@ app.post("/signup", (request, response) => {
                 email: newUser.email,
                 createdAt: new Date().toISOString(),
                 uid,
+                username: newUser.username,
             };
 
             return db.doc(`/users/${newUser.username}`).set(newUserCredentials);
